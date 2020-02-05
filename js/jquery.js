28711ddcb6f5d3a54dde80d0bcc4e349c9008624
CodingPenguin <email@example.com>
2020-02-04 20:04:09 -0600

    Hehe need to commit for colter

---
+++ js/jquery.js
@@ -0,0 +1,20 @@
+/*console.log("hi");
+
+var dropbtn = $("li.dropbtn");
+var dropContent = $(document);
+
+console.log(dropbtn.length);
+console.log(dropContent.length);
+$(dropbtn).click(function () {
+  console.log("click");
+  if(dropContent.hasClass("hidden")) {
+    dropContent.removeClass("hidden");
+    dropContent.addClass("visible");
+  }
+  else{
+    dropContent.removeClass("visible");
+    dropContent.addClass("hidden");
+    console.log("bet");
+  }
+});
+*/
